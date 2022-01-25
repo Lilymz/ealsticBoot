a6196069ed8c9c163a7c0c0232a8b37999bf3731
lilymz <2634083023@qq.com>
2022-01-25 22:23:41 +0800

    jpa

--- src/main/java/com/zjmx/elastic/ElasticBootApplication.java
+++ src/main/java/com/zjmx/elastic/ElasticBootApplication.java
@@ -1,5 +1,6 @@
 package com.zjmx.elastic;
 
+import com.zjmx.elastic.repository.SysUserRepository;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,8 @@ public class ElasticBootApplication {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(ElasticBootApplication.class, args);
 		RestHighLevelClient bean = run.getBean(RestHighLevelClient.class);
-		System.out.println(bean);
+		SysUserRepository sysUserRepository = run.getBean(SysUserRepository.class);
+		System.out.println(sysUserRepository.findAll());
 	}
 
 }
--- src/main/java/com/zjmx/elastic/repository/SysUserRepository.java
+++ src/main/java/com/zjmx/elastic/repository/SysUserRepository.java
@@ -1,4 +1,7 @@
 package com.zjmx.elastic.repository;
 
-public class SysUserRepository {
+import com.zjmx.elastic.domain.SysUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SysUserRepository extends CrudRepository<SysUser,Integer> {
 }
